09034f6ac45c079017de159a69502c2b355751a0
watanase <veltread@gmail.som>
2020-05-26 17:59:51 +0900

    initialize repository

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Carewrite
+  class Application < Rails::Application
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = [:en, :ja]
+    config.time_zone = "Tokyo"
+  end
+end
