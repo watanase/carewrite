20d469771b4bfe4a9673bfd4cc5f7cd3c403905c
watanase <melcyntier@gmail.com>
2020-06-15 22:15:53 +0900

    temp

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,11 +5,6 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    if @group.save
-      redirect_to company_path(current_company)
-    else
-      render :new
-    end
   end
 
   def show
