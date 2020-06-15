7407659352a294279244910bdda2b06c3e67fe94
watanase <melcyntier@gmail.com>
2020-06-16 06:21:18 +0900

    temp

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -47,7 +47,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      redirect_to :root
+      render :new_user
     end
   end
 
