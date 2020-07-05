7c2542379af8b49ba77e2ef73d56b3b0f7c44833
watanase <melcyntier@gmail.com>
2020-07-05 22:38:30 +0900

    temp

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_06_05_184253) do
     t.string "focus"
     t.text "content"
     t.datetime "datetime", default: -> { "CURRENT_TIMESTAMP" }
+    t.string "recorder"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
