7e84abf8cd687f037616dff4d07dd0ceef31cce7
watanase <melcyntier@gmail.com>
2020-07-22 19:58:37 +0900

    fix/rspec

--- spec/factories/users.spec.rb
+++ spec/factories/users.spec.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
+    name                  { 'fuga' }
     login_id              { 'abe' }
     password              { '00000000' }
     password_confirmation { '00000000' }
