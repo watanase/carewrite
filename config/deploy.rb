ae590d56cd84d66e51844483311a2ab91c7757a4
watanase <melcyntier@gmail.com>
2020-07-23 22:51:03 +0900

    fix/dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,6 +14,7 @@ set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/carewrite.pem']
+                  keepalive: true
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
